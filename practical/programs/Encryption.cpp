4936a338095f8f0c734638ed0cdfffaaf9d3378b
unknown <parth.kansara9898@gmail.com>
2022-09-05 22:29:17 +0530

    first commit

--- practical/programs/Encryption.cpp
+++ practical/programs/Encryption.cpp
@@ -4,59 +4,46 @@ char encode(char *encode_msg,int KEY)
 {
     int i;
     for(i=0;encode_msg[i]!='\0';i++)
-    {
         if(encode_msg[i]>='a' && encode_msg[i]<='z')
-        {
-            if(encode_msg[i]+KEY > 'z')
-            {
-                encode_msg[i]+=(KEY-26);
-                //encode_msg[i]=(encode_msg[i]-'a'+KEY)%26+'a';
-            }
-            else
-            {
-                encode_msg[i]+=(KEY);
-            }
-        }
-    }
+            encode_msg[i]=(encode_msg[i]-'a'+KEY)%26+'a';
     return encode_msg[i];
 }
 char decode(char *decode_msg,int KEY)
 {
     int i;
     for(i=0;decode_msg[i]!='\0';i++)
-    {
         if(decode_msg[i]>='a' && decode_msg[i]<='z')
-        {
-            if(decode_msg[i]-KEY < 'a')
-            {
-                decode_msg[i]-=(KEY-26);
-            }
-            else
-            {
-                decode_msg[i]-=(KEY);
-            }
-        }
-    }
+            encode_msg[i]=(encode_msg[i]-'a'+KEY)%26+'a';
     return decode_msg[i];
+}
+
+void bruteforce(char *msg)
+{
+    char temp[];
+    for(int i=0;i<25;i++)
+    {
+        strcmp(temp, msg);
+        decode(temp,i+1);
+        cout << "decode message for key " << i+1 << " is : " << temp << endl;
+    }
+
 }
 int main()
 {
     char msg[200],code_msg[200];
-    int key,key2;
+    int key;
+    
     cout << "Enter message : ";
     cin >> msg;
+
     cout << "Enter Key : ";
     cin >> key;
-    key%=26;
     encode(msg,key);
     cout << "Encoded Message Is : " << msg << endl;
 
-    cout << "\nEnter Decoded Message : " ;
+    cout << "\nEnter Message to Decoded: " ;
     cin >> code_msg;
-    cout << "Enter Key : ";
-    cin >> key2;
-    key2%=26;
-    decode(code_msg,key);
-    cout << "Decoded Message Is : " << code_msg << endl;
+
+    bruteforce(code_msg);
     return 0;
 }
